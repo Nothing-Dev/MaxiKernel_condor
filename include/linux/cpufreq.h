e6a2aa0903b6131fc8d13a74df35aad7ccae89ab
Nothing-Dev <alfredobigpapi2@gmail.com>
2015-08-29 11:19:49 -0430

    Fix using ondemandplus how default governor during compilation
    This fix is:
    In file included from drivers/cpufreq/cpufreq.c:22:0:
    drivers/cpufreq/cpufreq.c: In function ‘cpufreq_add_dev’:
    include/linux/cpufreq.h:392:37: error: ‘cpufreq_gov_ONDEMANDPLUS’ undeclared (first use in this function)
     #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ONDEMANDPLUS)
                                         ^
    drivers/cpufreq/cpufreq.c:1004:22: note: in expansion of macro ‘CPUFREQ_DEFAULT_GOVERNOR’
       policy->governor = CPUFREQ_DEFAULT_GOVERNOR;
                          ^
    include/linux/cpufreq.h:392:37: note: each undeclared identifier is reported only once for each function it appears in
     #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ONDEMANDPLUS)
                                         ^
    drivers/cpufreq/cpufreq.c:1004:22: note: in expansion of macro ‘CPUFREQ_DEFAULT_GOVERNOR’
       policy->governor = CPUFREQ_DEFAULT_GOVERNOR;
                          ^
      CC      sound/soc/codecs/wcd9xxx-common.o
    make[2]: *** [drivers/cpufreq/cpufreq.o] Error 1
    make[1]: *** [drivers/cpufreq] Error 2

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -389,7 +389,7 @@ extern struct cpufreq_governor cpufreq_gov_wheatley;
 #define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_wheatley)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
-#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ONDEMANDPLUS)
+#define CPUFREQ_DEFAULT_GOVERNOR  (&cpufreq_gov_ondemandplus)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ALUCARD)
 extern struct cpufreq_governor cpufreq_gov_alucard;
 #define CPUFREQ_DEFAULT_GOVERNOR (&cpufreq_gov_alucard)
